29be8fcdfbf46fbcb222eb6c4a469da4b4c36d51
SamEBae <baesammie@gmail.com>
2015-03-22 21:52:15 -0400

    added more charts now that the api supports more data

--- angle.js
+++ angle.js
@@ -1,105 +1,197 @@
+  $(document).ready(function() {
+      //time delay for the JSON to load from the api 
+      getData();
+      var delay = 3000;
+
+      setTimeout(function() {
+          d3.select('#angleChart svg').datum(angleDataJSON()).transition().duration(1000).call(angleChart.forceY([45, 270]).color(["#066464"]));
+          d3.select('#swingchart svg').datum(swingDataJSON()).transition().duration(1000).call(swingchart.forceY([0,300]));
+          d3.select('#stancechart svg').datum(stanceDataJSON()).transition().duration(1000).call(stancechart.forceY([0,30]));
+          d3.select('#stridechart svg').datum(strideDataJSON()).transition().duration(1000).call(stridechart.forceY([0,400]));
+          //d3.select('#swingchart svg').datum(angleDataJSON()).transition().duration(1000).call(swingchart.forceY([0]));
+      }, delay);
+  });
+
   //angle chart 
-  var testchart = nv.models.multiBarChart().margin({left: 100})
-                    .tooltip(function(key, x, y, e, graph) {
-                      return '<h3>x&#772:'+y+'</h3>' +
-                             '<p>' +  y + '° at ' + x + '</p>';
-                    });
-  testchart.yAxis.axisLabel('Angle°');
-  testchart.xAxis.axisLabel('Time(hour:min:second)');
-  testchart.reduceXTicks(false);
+  var angleChart = nv.models.multiBarChart().margin({
+          left: 100
+      })
+      .tooltip(function(key, x, y, e, graph) {
+          return '<h3>x&#772:' + y + '</h3>' +
+              '<p>' + y + '° at ' + x + '</p>';
+      });
+  angleChart.yAxis.axisLabel('Angle°');
+  angleChart.xAxis.axisLabel('Time(hour:min:second)');
+  angleChart.reduceXTicks(false);
 
   //array in which to store the data from the JSON response
-  var AngleDataLeftFoot   =[];
-  var AngleDataRightFoot  =[];
+  var AngleDataLeftFoot   = [];
+  var AngleDataRightFoot  = [];
+  var swingData           = [];
+  var strideData          = [];
+  var stanceData          = [];
 
-  function testData(){
-    $.getJSON('http://ioaklym.herokuapp.com/steps', function(data) {
-        console.log(data);
-        //loop and see see ach different data entry
-        for (var i = 0; i < data.length; i++) {
-          //storing the data
-          console.log(data[i].angle);
-          console.log((data[i].timestamp).substring(12,19));
-          //push it to array
-          AngleDataLeftFoot.push({x: (data[i].timestamp).substring(12,19),  y: data[i].angle});
-          AngleDataRightFoot.push({x:(data[i].timestamp).substring(12,19),  y: Math.random()+125+(10*randomSign())})
-        };
-    });
+  function getData() {
+      $.getJSON('http://ioaklym.herokuapp.com/steps', function(data) {
+          console.log(data);
+          //loop and see see ach different data entry
+          for (var i = 0; i < data.length; i++) {
+              //storing the data
+              console.log(data[i].angle);
+              console.log((data[i].timestamp).substring(12, 19));
+              console.log(data[i].swing);
+              //push it to array
+
+              if (data[i].angle) {
+                  AngleDataLeftFoot.push({
+                      x: (data[i].timestamp).substring(12, 19),
+                      y: data[i].angle
+                  });
+                  AngleDataRightFoot.push({
+                      x: (data[i].timestamp).substring(12, 19),
+                      y: Math.random() + 125 + (10 * randomSign())
+                  });
+              }
+              if(typeof data[i].swing !== 'undefined'){
+                  swingData.push({
+                      x: (data[i].timestamp).substring(12, 19),
+                      y: data[i].swing
+                  });
+              }
+              if(data[i].stride){
+                  strideData.push({
+                      x: (data[i].timestamp).substring(12, 19),
+                      y: data[i].stride
+                  });
+              }
+              if(data[i].stance){
+                  stanceData.push({
+                      x: (data[i].timestamp).substring(12, 19),
+                      y: data[i].stance
+                  });
+              }
+
+          };
+      });
   }
 
-  $(document).ready(function (){
-    //time delay for the JSON to load from the api 
-    testData(); 
-    var delay = 3000;
-    
-    //nvd3 calls this function to retreive the data
-    function data() {
-      return [
-        {
+  function randomSign() {
+      if (Math.random() > 0.75) return -1;
+      return 1;
+  }
+
+  //data for angle chart
+  function angleDataJSON() {
+      return [{
           values: AngleDataLeftFoot,
           key: 'Left Foot',
           color: '#288E8E'
-        },
-        {
+      }, {
           values: AngleDataRightFoot,
           key: 'Right foot',
           color: '#9CE3E3'
-        }
-      ];
-    }
+      }];
+  }
 
-    setTimeout(function(){
-      d3.select('#testchart svg').datum(data()).transition().duration(1000).call(testchart.forceY([90]).color(["#066464"]));
-    },delay);
+  //swing chart
+  var swingchart = nv.models.multiBarChart().margin({
+      left: 100
   });
 
-  //favors right foot. 
-  function randomSign(){
-    if(Math.random()>0.75)return -1;
-    return 1;
-  }
-  
-  // creates the download link
-  var jsonData = (function () {
-    var json = null;
-    $.ajax({
-        'async': false,
-        'global': false,
-        'url': 'http://ioaklym.herokuapp.com/steps',
-        'dataType': "json",
-        'success': function (data) {
-            json = data;
-        }
-    });
-    return json;
-  })();
+  swingchart.yAxis.axisLabel('Swing');
+  swingchart.xAxis.axisLabel('Time(hour:min:second)');
 
+  //data for the swing chart
+  function swingDataJSON(){
+      return [{
+          values: swingData,
+          key: 'Swing',
+          color: '#5C97BF'
+      }];
+  }
+
+  //stance chart
+  var stancechart = nv.models.multiBarChart().margin({
+      left: 100
+  });
+
+  stancechart.yAxis.axisLabel('Stance');
+  stancechart.xAxis.axisLabel('Time(hour:min:second)');
+
+  //data for the swing chart
+  function stanceDataJSON(){
+      return [{
+          values: stanceData,
+          key: 'Stance',
+          color: '#9CE3E3'
+      }];
+  }
+
+  //stride chart
+  var stridechart = nv.models.multiBarChart().margin({
+      left: 100
+  });
+
+  stridechart.yAxis.axisLabel('Stance');
+  stridechart.xAxis.axisLabel('Time(hour:min:second)');
+
+  //data for the swing chart
+  function strideDataJSON(){
+      return [{
+          values: strideData,
+          key: 'Stride',
+          color: '#288E8E'
+      }];
+  }
+
+
+  //for the download link
+  var jsonData = (function() {
+      var json = null;
+      $.ajax({
+          'async': false,
+          'global': false,
+          'url': 'http://ioaklym.herokuapp.com/steps',
+          'dataType': "json",
+          'success': function(data) {
+              json = data;
+          }
+      });
+      return json;
+  })();
   var data = jsonData;
   var json = JSON.stringify(data);
-  var blob = new Blob([json], {type: "application/json"});
-  var url  = URL.createObjectURL(blob);
-
-  var a = document.createElement('a');
+  var blob = new Blob([json], {
+      type: "application/json"
+  });
+  var url = URL.createObjectURL(blob);
+  var a   = document.createElement('a');
   a.download    = "angle.json";
   a.href        = url;
   a.textContent = "Download angle data";
-
   document.getElementById('downloadAngle').appendChild(a);
+  var show = false;
+  $("#menu").click(function() {
+      if (!show) {
+          show = true;
+          $('.row').fadeIn(1200);
+          $('#menu').tipsy({
+              show: 'false'
+          });
+      } else {
+          show = false;
+          $('.row').fadeOut(1200);
+          $('#menu').tipsy({
+              show: 'true'
+          });
+      }
 
-
-  var show=false;
-  $("#menu").click(function (){
-    if(!show){
-      show = true;
-      $('.row').fadeIn(1200);
-      $('#menu').tipsy({show:'false'});
-    }else{
-      show = false;
-      $('.row').fadeOut(1200);
-      $('#menu').tipsy({show:'true'});
-    }
-    
   })
 
-$('#menu').tipsy({trigger: 'manual', title: 'original-title', gravity:'w'});
-$('#menu').tipsy('show');
+  //tooltip using tipsy.js library
+  $('#menu').tipsy({
+      trigger: 'manual',
+      title: 'original-title',
+      gravity: 'w'
+  });
+  $('#menu').tipsy('show');
--- pressure.js
+++ pressure.js
@@ -1,50 +1,53 @@
-//tool tip at the top
+  var pressurechart = nv.models.lineChart().margin({
+      left: 100
+  });
 
-$('#menu').tipsy({trigger: 'manual', title: 'original-title', gravity:'w'});
-$('#menu').tipsy('show');
+  pressurechart.yAxis.axisLabel('Pressure');
+  pressurechart.xAxis.axisLabel('Time(hour:min:second)');
+  d3.select('#pressurechart svg').datum(pressureData()).transition().duration(1000).call(pressurechart.forceY([0, 1]));
 
-var pressurechart = nv.models.lineChart().margin({left: 100});
+  function pressureData() {
+      var point1 = [];
+      var point2 = [];
+      var point3 = [];
+      var point4 = [];
 
-pressurechart.yAxis.axisLabel('Pressure');
-pressurechart.xAxis.axisLabel('Time(hour:min:second)');
+      for (var i = 0; i < 100; i++) {
+          point1.push({
+              x: i,
+              y: Math.sin(i / 10) + Math.random()
+          });
+          point2.push({
+              x: i,
+              y: Math.cos(i / 10) + Math.random()
+          });
+          point3.push({
+              x: i,
+              y: Math.sin(i / 10) + Math.random()
+          });
+          point4.push({
+              x: i,
+              y: Math.cos(i / 10) + Math.random()
+          });
+      }
 
-
-d3.select('#pressurechart svg').datum(pressureData()).transition().duration(1000).call(pressurechart.forceY([0,1]));
-
-
-function pressureData() {
-  var point1 =[];
-  var point2 =[];
-  var point3 =[];
-  var point4 =[];
-
-  for (var i = 0; i < 100; i++) {
-    point1.push({x: i, y: Math.sin(i/10)+Math.random()});
-    point2.push({x: i, y: Math.cos(i/10)+Math.random()});
-    point3.push({x: i, y: Math.sin(i/10)+Math.random()});
-    point4.push({x: i, y: Math.cos(i/10)+Math.random()});
+      return [{
+          values: point1,
+          key: 'Pressure Point 1',
+          color: '#5C97BF'
+      }, {
+          values: point2,
+          key: 'Pressure Point 2',
+          color: '#EF4836'
+      }, {
+          values: point3,
+          key: 'Pressure Point 3',
+          color: '#9A12B3'
+      }, {
+          values: point4,
+          key: 'Pressure Point 4',
+          color: '#1E824C'
+      }];
   }
 
-  return [
-    {
-      values: point1,
-      key: 'Pressure Point 1',
-      color: '#5C97BF'
-    },
-    {
-      values: point2,
-      key: 'Pressure Point 2',
-      color: '#EF4836'
-    },
-    {
-      values: point3,
-      key: 'Pressure Point 3',
-      color: '#9A12B3'
-    },
-    {
-      values: point4,
-      key: 'Pressure Point 4',
-      color: '#1E824C'
-    }
-  ];
-}
+  
